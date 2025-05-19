253b2597e6893381b68c4c3baede267c815dc221
Asaph Arruda <asapharruda@gmail.com>
2025-05-19 15:25:05 -0300

    Mud: Adicionando um novo atributo para o cliente - email.

--- monitorarClientes/src/main/java/com/monitorar/monitorarClientes/model/Cliente.java
+++ monitorarClientes/src/main/java/com/monitorar/monitorarClientes/model/Cliente.java
@@ -7,6 +7,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 
 @Entity
 @Table(name = "tb_cliente")
@@ -14,19 +16,30 @@ public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank(message = "O nome é obrigatório")
     private String nome;
+
+    @NotBlank(message = "O endereço é obrigatório")
     private String endereco;
+
+    @NotBlank(message = "O telefone é obrigatório")
     private String telefone;
+
+    @Email(message = "Email inválido")
+    @NotBlank(message = "O email é obrigatório")
+    private String email;
     private LocalDateTime dt_instalacao;
     
     public Cliente() {
     }
     
-    public Cliente(Long id, String nome, String endereco, String telefone, LocalDateTime dt_instalacao) {
+    public Cliente(Long id, String nome, String endereco, String telefone, String email, LocalDateTime dt_instalacao) {
         this.id = id;
         this.nome = nome;
         this.endereco = endereco;
         this.telefone = telefone;
+        this.email = email;
         this.dt_instalacao = dt_instalacao;
     }
     public void setNome(String nome) {
@@ -37,6 +50,12 @@ public class Cliente {
     }
     public void setTelefone(String telefone) {
         this.telefone = telefone;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getEmail() {
+        return email;
     }
     public void setDt_instalacao(LocalDateTime dt_instalacao) {
         this.dt_instalacao = dt_instalacao;
