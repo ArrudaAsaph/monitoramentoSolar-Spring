e6091f0a87643d9f8d7184abfbfe0fe06afb5a7d
Asaph Arruda <asapharruda@gmail.com>
2025-05-22 17:04:23 -0300

    Mud: Corrigindo atributos da model, com base no Diagrama de Classe

--- monitorarClientes/src/main/java/com/monitorar/monitorarClientes/model/Dimensionamento.java
+++ monitorarClientes/src/main/java/com/monitorar/monitorarClientes/model/Dimensionamento.java
@@ -1,6 +1,8 @@
 package com.monitorar.monitorarClientes.model;
 
 
+import java.time.LocalDate;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -27,66 +29,66 @@ public class Dimensionamento {
     @JoinColumn(name = "local_id")
     private Local local;
     
-    private float consumoMed;
-
-    private float potSistema;
-
     @OneToOne
     @JoinColumn(name = "kit_id")
     private Kit kit;
 
+    private float consumoMed;
+
+    private LocalDate dt_instalacao;
+
     public Dimensionamento() {
     }
 
-    public Dimensionamento(Long id, Cliente cliente, Local local, float consumoMed, float potSistema, Kit kit) {
+    public Dimensionamento(Long id, Cliente cliente, Local local, Kit kit, float consumoMed, LocalDate dt_instalacao) {
         this.id = id;
         this.cliente = cliente;
         this.local = local;
-        this.consumoMed = consumoMed;
-        this.potSistema = potSistema;
         this.kit = kit;
-    }
-
-    public void setCliente(Cliente cliente) {
-        this.cliente = cliente;
-    }
-
-    public void setLocal(Local local) {
-        this.local = local;
-    }
-
-    public void setConsumoMed(float consumoMed) {
         this.consumoMed = consumoMed;
-    }
-
-    public void setSistema(float potSistema) {
-        this.potSistema = potSistema;
-    }
-
-    public void setKit(Kit kit) {
-        this.kit = kit;
+        this.dt_instalacao = dt_instalacao;
     }
 
     public Cliente getCliente() {
         return cliente;
     }
 
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
     public Local getLocal() {
         return local;
     }
 
-    public float getConsumoMed() {
-        return consumoMed;
-    }
-
-    public float getSsistema() {
-        return potSistema;
+    public void setLocal(Local local) {
+        this.local = local;
     }
 
     public Kit getKit() {
         return kit;
     }
 
-    
+    public void setKit(Kit kit) {
+        this.kit = kit;
+    }
+
+    public float getConsumoMed() {
+        return consumoMed;
+    }
+
+    public void setConsumoMed(float consumoMed) {
+        this.consumoMed = consumoMed;
+    }
+
+    public LocalDate getDt_instalacao() {
+        return dt_instalacao;
+    }
+
+    public void setDt_instalacao(LocalDate dt_instalacao) {
+        this.dt_instalacao = dt_instalacao;
+    }
+
+
 
 }
