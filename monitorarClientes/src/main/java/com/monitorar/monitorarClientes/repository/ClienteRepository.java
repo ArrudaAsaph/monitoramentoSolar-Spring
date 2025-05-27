5e40bf2dfa9ca32e05dc5a6ee005e26879e4cdae
Asaph Arruda <asapharruda@gmail.com>
2025-05-27 17:00:33 -0300

    Add: Cliente

--- monitorarClientes/src/main/java/com/monitorar/monitorarClientes/repository/ClienteRepository.java
+++ monitorarClientes/src/main/java/com/monitorar/monitorarClientes/repository/ClienteRepository.java
@@ -1,11 +1,10 @@
 package com.monitorar.monitorarClientes.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.monitorar.monitorarClientes.model.Cliente;
 
-@Repository
-public interface ClienteRepository extends JpaRepository<Cliente, Long> {
-    
+public interface ClienteRepository extends JpaRepository<Cliente, Long>{
+    boolean existsByEmail(String email);
+    boolean existsByEndereco(String endereco);
 }
