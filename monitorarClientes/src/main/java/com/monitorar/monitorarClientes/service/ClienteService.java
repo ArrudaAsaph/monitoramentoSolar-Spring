78565b5b20b95e916aaaaa8b987e032bf20b1fde
Asaph Arruda <asapharruda@gmail.com>
2025-05-27 17:00:58 -0300

    Add: Cliente

--- monitorarClientes/src/main/java/com/monitorar/monitorarClientes/service/ClienteService.java
+++ monitorarClientes/src/main/java/com/monitorar/monitorarClientes/service/ClienteService.java
@@ -1,44 +1,62 @@
 package com.monitorar.monitorarClientes.service;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
 import com.exception.NaoEncontrado;
+import com.monitorar.monitorarClientes.dto.ClienteDTO;
 import com.monitorar.monitorarClientes.model.Cliente;
 import com.monitorar.monitorarClientes.repository.ClienteRepository;
 
 @Service
 public class ClienteService {
-    
-    private final ClienteRepository clienteRepository;
+    private ClienteRepository clienteRepository;
+
 
     public ClienteService(ClienteRepository clienteRepository) {
         this.clienteRepository = clienteRepository;
     }
 
-    public List<Cliente> listarCliente() {
-        return clienteRepository.findAll(); //Listar todos os clientes
-    }
-
-    public Optional<Cliente> buscarPorId(Long id) {
-        if (!clienteRepository.existsById(id)) {
-            throw new NaoEncontrado("Cliente nao existente");
-    
+    public Cliente save(ClienteDTO dto) {
+        if (clienteRepository.existsByEmail(dto.getEmail())){
+            throw new IllegalArgumentException("Já existe um cliente com este email.");
         }
-        return clienteRepository.findById(id);
-        
+        Cliente cliente = new Cliente();
+        cliente.setNome(dto.getNome());
+        cliente.setEmail(dto.getEmail());
+        cliente.setTelefone(dto.getTelefone());
+        cliente.setEndereco(dto.getEndereco());
+        return clienteRepository.save(cliente);
     }
 
-    public Cliente save(Cliente cliente) {
+    public List<Cliente> findAll() {
+        return clienteRepository.findAll();
+    }
+
+    public Cliente findById(Long id) {
+        return clienteRepository.findById(id)
+            .orElseThrow(() -> new NaoEncontrado("Cliente não Encontrado!"));
+    }
+
+    public Cliente update(Long id, ClienteDTO dto) {
+        Cliente cliente = findById(id);
+
+        if (!cliente.getEmail().equals(dto.getEmail()) && clienteRepository.existsByEmail(dto.getEmail())) {
+            throw new IllegalArgumentException("Já existe um cliente com este email.");
+            
+        }
+        cliente.setNome(dto.getNome());
+        cliente.setEmail(dto.getEmail());
+        cliente.setEndereco(dto.getEndereco());
+        cliente.setTelefone(dto.getTelefone());
+
         return clienteRepository.save(cliente);
     }
 
     public void delete(Long id) {
-        if (!clienteRepository.existsById(id)) {
-            throw new NaoEncontrado("Cliente nao existente");
-        }
-        clienteRepository.deleteById(id);
+        Cliente cliente = findById(id);
+        clienteRepository.delete(cliente);
     }
+
 }
