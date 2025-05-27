129f25317c6a3eddd7a6f17e4a91740c28d43d64
Asaph Arruda <asapharruda@gmail.com>
2025-05-27 17:02:34 -0300

    Change

--- monitorarClientes/src/main/java/com/monitorar/monitorarClientes/model/Cliente.java
+++ monitorarClientes/src/main/java/com/monitorar/monitorarClientes/model/Cliente.java
@@ -1,76 +1,56 @@
 package com.monitorar.monitorarClientes.model;
 
-
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 
-@Entity
-@Table(name = "tb_cliente")
+@Entity // Uma entidade
+@Table(name = "tb_cliente") // Essa entidade irá se tornar uma tabela
 public class Cliente {
+    
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Gerar os identificadores sequenciais
     private Long id;
-
-    @NotBlank(message = "O nome é obrigatório")
     private String nome;
-    
-    @Email(message = "Email inválido")
-    @NotBlank(message = "O email é obrigatório")
     private String email;
-    
-    @NotBlank(message = "O endereço é obrigatório")
-    private String endereco;
-    
-    @NotBlank(message = "O telefone é obrigatório")
     private String telefone;
-    
+    private String endereco;
+
     public Cliente() {
     }
-    
-    public Cliente(Long id, String nome, String email, String telefone, String endereco ) {
+    public Cliente(Long id, String nome, String email, String telefone, String endereco) {
         this.id = id;
         this.nome = nome;
         this.email = email;
         this.telefone = telefone;
         this.endereco = endereco;
-        
+    }
+    public String getNome() {
+        return nome;
     }
     public void setNome(String nome) {
         this.nome = nome;
     }
-
+    public String getEmail() {
+        return email;
+    }
     public void setEmail(String email) {
         this.email = email;
     }
-
+    public String getTelefone() {
+        return telefone;
+    }
     public void setTelefone(String telefone) {
         this.telefone = telefone;
     }
-
+    public String getEndereco() {
+        return endereco;
+    }
     public void setEndereco(String endereco) {
         this.endereco = endereco;
     }
 
-    public String getNome() {
-        return nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public String getEndereco() {
-        return endereco;
-    }
-  
+    
 }
