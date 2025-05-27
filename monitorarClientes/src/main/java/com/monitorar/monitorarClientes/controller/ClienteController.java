74dd65991e0d745731388ecdad9c980aa43ad8af
Asaph Arruda <asapharruda@gmail.com>
2025-05-27 16:58:51 -0300

    Mud: Refazendo

--- monitorarClientes/src/main/java/com/monitorar/monitorarClientes/controller/ClienteController.java
+++ monitorarClientes/src/main/java/com/monitorar/monitorarClientes/controller/ClienteController.java
@@ -1,41 +1,57 @@
 package com.monitorar.monitorarClientes.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.monitorar.monitorarClientes.dto.ClienteDTO;
 import com.monitorar.monitorarClientes.model.Cliente;
 import com.monitorar.monitorarClientes.service.ClienteService;
 
-import java.util.List;
-
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
-
-@CrossOrigin(origins = "*")
 @RestController
-@RequestMapping("/cliente")
+@CrossOrigin(origins = "*")
+@RequestMapping("/api/clientes")
 public class ClienteController {
-    
     private final ClienteService clienteService;
 
     public ClienteController(ClienteService clienteService) {
         this.clienteService = clienteService;
     }
 
-    @GetMapping
-    public List<Cliente> listarCliente() {
-        return clienteService.listarCliente();
-    }
-
     @PostMapping
-    public Cliente saveCliente(@RequestBody Cliente cliente) {
-        return clienteService.save(cliente);
+    public ResponseEntity<Cliente> criarCliente(@RequestBody ClienteDTO dto) {
+        Cliente cliente = clienteService.save(dto);
+        return new ResponseEntity<>(cliente, HttpStatus.CREATED);
     }
-    
-    
 
+    @GetMapping
+    public ResponseEntity<List<Cliente>> listarCliente() {
+        List<Cliente> clientes = clienteService.findAll();
+        return ResponseEntity.ok(clientes);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Cliente> atualizarCliente(@PathVariable Long id, @RequestBody ClienteDTO dto) {
+        Cliente clienteAtualizado = clienteService.update(id, dto);
+        return ResponseEntity.ok(clienteAtualizado);
+    }
+
+    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarCliente(@PathVariable Long id) {
+        clienteService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    
 }
